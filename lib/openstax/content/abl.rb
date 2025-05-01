198287f5fa469db15ef3fda20a562f4e7ebeab80
Dante Soares <dante.m.soares@rice.edu>
2025-05-01 11:51:25 -0500

    Moved code to extract page slugs from Exercises to here

--- lib/openstax/content/abl.rb
+++ lib/openstax/content/abl.rb
@@ -2,6 +2,10 @@ require_relative 'archive'
 require_relative 'book'
 
 class OpenStax::Content::Abl
+  # Check back this many archive versions
+  # If there are more than this number of archive versions still building, errors will happen
+  DEFAULT_MAX_ARCHIVE_ATTEMPTS = 5
+
   def initialize(url: nil)
     @url = url
   end
@@ -32,6 +36,67 @@ class OpenStax::Content::Abl
         slug: book[:slug],
         committed_at: book[:committed_at]
       )
+    end
+  end
+
+  def each_book_with_previous_archive_version_fallback(max_attempts: DEFAULT_MAX_ARCHIVE_ATTEMPTS, &block)
+    raise ArgumentError, 'no block given' if block.nil?
+    raise ArgumentError, 'given block must accept the book as its first argument' if block.arity == 0
+
+    books = OpenStax::Content::Abl.new.books
+    attempt = 1
+
+    until books.empty?
+      previous_version = nil
+      previous_archive = nil
+      retry_books = []
+
+      books.each do |book|
+        begin
+          block.call book
+        rescue StandardError => exception
+          raise exception if attempt >= max_attempts
+
+          # Sometimes books in the latest archive fails to load (when the new version is still building)
+          # Retry with an earlier version of archive, if possible
+          previous_version ||= book.archive.previous_version
+
+          if previous_version.nil?
+            # There are no more earlier archive versions
+            raise exception
+          else
+            previous_archive ||= OpenStax::Content::Archive.new version: previous_version
+
+            retry_book = OpenStax::Content::Book.new(
+              archive: previous_archive,
+              uuid: book.uuid,
+              version: book.version,
+              slug: book.slug,
+              min_code_version: book.min_code_version,
+              committed_at: book.committed_at
+            )
+
+            # If the book requires an archive version that hasn't finished building yet, don't include it
+            retry_books << retry_book if retry_book.valid?
+          end
+        end
+      end
+
+      books = retry_books
+      attempt += 1
+    end
+  end
+
+  def slugs_by_page_uuid(max_attempts: DEFAULT_MAX_ARCHIVE_ATTEMPTS)
+    @slugs_by_page_uuid ||= {}.tap do |hash|
+      each_book_with_previous_archive_version_fallback(max_attempts: max_attempts) do |book|
+        book.all_pages.each do |page|
+          hash[page.uuid] ||= []
+          hash[page.uuid] << { book: book.slug, page: page.slug }
+        end
+      end
+
+      hash.each { |uuid, slugs| slugs.uniq! }
     end
   end
 end
--- spec/openstax/content/abl_spec.rb
+++ spec/openstax/content/abl_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe OpenStax::Content::Abl, vcr: VCR_OPTS do
   it "can return the ABL's digest" do
     expect(abl.digest).to eq '1af6dd1b7c006c91563d3a07598bc2a117b53ae2f89a54c7d6100a3f574e6fe6'
   end
+
+  it 'can search previous archive versions when the latest book version is not found' do
+    abl.each_book_with_previous_archive_version_fallback do |book|
+      next unless book.uuid == '185cbf87-c72e-48f5-b51e-f14f21b5eabd'
+      book.all_pages
+    end
+  end
+
+  it 'can return a map of all page slugs by uuid' do
+    expect(abl.slugs_by_page_uuid.size).to eq(24385)
+  end
 end
